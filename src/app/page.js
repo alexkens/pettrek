a6666c140fad2f896fabfcd1e803fdd3a6796d09
alexk <alexkens93@gmail.com>
2025-10-14 00:01:06 +0200

    progress on background image

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-        <div className="h-600 w-full bg-[url(/stock_dog.jpg)]">
-            <nav>
-                <img className="h-10" src="/pettrek_logo.png"></img>
-            </nav>
-            <h1>Welcome to Pettrek!</h1>
-            <h2>Your intergalactical Dog Walking Site!</h2>
-            <button>Tool</button>
+        <div className="flex flex-col items-center">
+            <div id="hero" className="w-[80%]">
+                <nav>
+                    <img className="h-10" src="/pettrek_logo.png"></img>
+                </nav>
+                <h1>Welcome to Pettrek!</h1>
+                <h2>Your intergalactical Dog Walking Site!</h2>
+                <div className="h-100"></div>
+                <button>Tool</button>
+            </div>
         </div>
     );
 }
