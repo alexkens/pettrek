c957faed66d75b47716436847b4ff1bc2c98627a
alexk <alexkens93@gmail.com>
2025-10-19 22:55:26 +0200

    maplibre integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,12 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="apple-mobile-web-app-title" content="Pettrek" />
+
+        <script src="https://tiles.versatiles.org/assets/lib/maplibre-gl/maplibre-gl.js"></script>
+        <link href="https://tiles.versatiles.org/assets/lib/maplibre-gl/maplibre-gl.css" rel="stylesheet" />
+
+        <link rel='stylesheet' href='https://unpkg.com/maplibre-gl@5.9.0/dist/maplibre-gl.css' />
+        <script src='https://unpkg.com/maplibre-gl@5.9.0/dist/maplibre-gl.js'></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
--- src/app/planner/page.js
+++ src/app/planner/page.js
@@ -1,5 +1,38 @@
+'use client'
+
+import { useEffect, useRef } from 'react';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
 export default function Planner() {
     return (
-        <div>what up</div>
+        <div>
+            <div>dumelang my banana pie jizzers!</div>
+            <Map />
+        </div>
+    );
+}
+
+function Map() {
+    const mapContainer = useRef(null);
+
+    useEffect(() => {
+        if (!mapContainer.current) return;
+
+        const map = new maplibregl.Map({
+            container: mapContainer.current,
+            style: 'https://tiles.versatiles.org/assets/styles/colorful/style.json',
+            center: [0, 0],
+            zoom: 3
+        });
+
+        return () => map.remove(); // cleanup on unmount
+    }, []);
+
+    return (
+        <div
+            ref={mapContainer}
+            style={{ width: '100%', height: '80vh' }}
+        />
     );
 }
